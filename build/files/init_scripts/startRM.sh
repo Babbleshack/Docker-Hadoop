58c6920a0cffb463b8b18b3ed8102f9bc1e816e7
babbleshack <dcrl94@gmail.com>
2020-03-22 19:34:19 +0000

    Fix HA, and write router vars in startRM.sh

--- build/files/init_scripts/startRM.sh
+++ build/files/init_scripts/startRM.sh
@@ -3,13 +3,7 @@ echo "SubCluster $CLUSTER_ID"
 /etc/init.d/ssh start
 echo "Setting federated vars..."
 /hadoop/scripts/writeFederatedVars.sh
-if [ -z $ROUTER ]
-then 
-	echo "Router var not set..."
-else
-	echo "Starting router..."
-	$HADOOP_HOME/bin/yarn --daemon start router
-fi
+
 echo "Configuring workers file"
 cp -fv /workers/workers /opt/hadoop/etc/hadoop/workers
 sed --in-place "s/<\!--CLUSTER-ID-VALUE-->/$CLUSTER_ID/g" $HADOOP_HOME/etc/hadoop/workers
@@ -18,7 +12,16 @@ echo "Starting resourcemanager..."
 $HADOOP_HOME/sbin/start-yarn.sh --daemon start resourcemanager
 echo "Starting historyserver..."
 $HADOOP_HOME/bin/mapred --daemon start historyserver
-sleep 3
+
+echo "Waiting for RM's to start"
+sleep 5
+if [ -z $ROUTER ]
+then 
+	echo "Router var not set..."
+else
+	echo "Starting router..."
+	$HADOOP_HOME/bin/yarn --daemon start router
+fi
 cat /opt/hadoop/logs/hadoop-root-resourcemanager-yarn-master-0.log
 while true
 do
--- build/files/init_scripts/writeFederatedVars.sh
+++ build/files/init_scripts/writeFederatedVars.sh
@@ -2,4 +2,5 @@ C_ID=${CLUSTER_ID:-"hadoop-cluster"}
 EPOCH_VALUE=$((1000 + RANDOM % 100000))
 sed --in-place "s/<\!--CLUSTER-ID-VALUE-->/$CLUSTER_ID/g" $HADOOP_HOME/etc/hadoop/yarn-site.xml
 sed --in-place "s/<\!--RESOURCEMANAGER-EPOCH-VALUE-->/$EPOCH_VALUE/g" $HADOOP_HOME/etc/hadoop/yarn-site.xml
+sed --in-place "s/<\!--CLUSTER-ID-VALUE-->/$CLUSTER_ID/g" /hadoop/federation/router/yarn-site.xml
 
