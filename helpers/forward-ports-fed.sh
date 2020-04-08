18283fb0c6b3a5c5257cc552b3b0d31451710e6f
babbleshack <dcrl94@gmail.com>
2020-04-08 13:34:39 +0100

    update ports

--- helpers/forward-ports-fed.sh
+++ helpers/forward-ports-fed.sh
@@ -7,6 +7,7 @@ CLUSTER_C=yarn-subcluster-c
 ROUTER_WEB=8089
 RM_WEB=8088
 JH_WEB=19888
-kubectl port-forward -n $CLUSTER_A svc/yarn-master-service $ROUTER_WEB $RM_WEB $JH_WEB --address 0.0.0.0 & 
+TS_WEB=8188
+kubectl port-forward -n $CLUSTER_A svc/yarn-master-service $TS_WEB $ROUTER_WEB $RM_WEB $JH_WEB --address 0.0.0.0 & 
 kubectl port-forward -n $CLUSTER_B svc/yarn-master-service 8099:$ROUTER_WEB 8098:$RM_WEB 19898:$JH_WEB --address 0.0.0.0 & 
 kubectl port-forward -n $CLUSTER_C svc/yarn-master-service 8109:$ROUTER_WEB 8108:$RM_WEB 19908:$JH_WEB --address 0.0.0.0 
